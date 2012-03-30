6e6056f38589157b5871d3a078497550220b4294
Jenna J E Lindh <jelindh@svm-19.cs.helsinki.fi>
2012-03-30 22:01:44 +0300

    jee

--- kysely.php
+++ kysely.php
@@ -13,13 +13,10 @@
              $yhteys = db::getDB();
              echo "<h1>".$_GET["kysely"]."</h1>";
 
-             $nimi = $_GET["kysely"];
-             $idkysely = $yhteys->prepare('select kurssikyselyid from kurssikysely where kknimi = ?');
+             $idkysely = $yhteys->prepare('select kysymys from kysymys inner join kurssikysely on kysymys.kurssikyselyid = kurssikysely.kurssikyselyid where kknimi = ?');
              $idkysely->execute(array($_GET["kysely"]));
-            
-	     $kysely = $yhteys->prepare('select kysymys from kysymys where kurssikyselyid = ?');
-             $kysely->execute(array($idkysely));
-             $tulokset = $kysely->fetchAll();
+
+             $tulokset = $idkysely->fetchAll();
 
 	     foreach($tulokset as $tulos) {
                echo $tulos['kysymys']."</br>";
