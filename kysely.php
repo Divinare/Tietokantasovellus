cac42cc64952a8f89a1293e59154f0c96986f598
Jenna J E Lindh <jelindh@svm-19.cs.helsinki.fi>
2012-03-30 21:42:12 +0300

    yhyy

--- kysely.php
+++ kysely.php
@@ -12,5 +12,17 @@
           <?php
              $yhteys = db::getDB();
              echo "<h1>".$_GET["kysely"]."</h1>";
+
+             $nimi = $_GET["kysely"];
+             $idkysely = $yhteys->prepare('select kurssikyselyid from kurssikysely where kknimi = ?');
+             $idkysely->execute(array($_GET["kysely"]));
+            
+	     $kysely = $yhteys->prepare('select kysymys from kysymys where kurssikyselyid = ?');
+             $kysely->execute(array($idkysely));
+             $tulokset = $kysely->fetchAll();
+
+	     foreach($tulokset as $tulos) {
+               echo $tulos['kysymys']."</br>";
+             }
             ?>
        </body>
