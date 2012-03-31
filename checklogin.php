b1fa1de8108f57119fdacf2db201e71f5a7587d4
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-03-31 22:32:18 +0300

    paivitys

--- checklogin.php
+++ checklogin.php
@@ -7,6 +7,10 @@
 </head>
      <body>
         <?php
+
+        $email = $_POST['email'];
+        $salasana = $_POST['salasana'];
+
         $yhteys = db::getDB();
 
         $sql = 'SELECT * FROM henkilo WHERE email='.$email.' and salasana = '.$salasana;
@@ -25,9 +29,6 @@
         //mysql_select_db("$db_name") or die("cannot select DB");
 
         // username and password sent from form
-        $email = $_POST['email'];
-        $salasana = $_POST['salasana'];
-
         // To protect MySQL injection (more detail about MySQL injection)
         //$myusername = stripslashes($myusername);
         //$mypassword = stripslashes($mypassword);
@@ -35,19 +36,18 @@
         //$mypassword = mysql_real_escape_string($mypassword);
 
         //$sql = "SELECT * FROM $tbl_name WHERE username='$myusername' and password='$mypassword'";
-        $result = mysql_query($sql);
+        $result = pg_query($sql);
 
         // Mysql_num_row is counting table row
-        $count = mysql_num_rows($result);
+        $count = pg_num_rows($result);
         // If result matched $myusername and $mypassword, table row must be 1 row
-
         if ($count == 1) {
             // Register $myusername, $mypassword and redirect to file "login_success.php"
             session_register("email");
             session_register("salasana");
             header("location:login_success.php");
         } else {
-            echo "Wrong Username or Password";
-        }
+            echo $count . " testi " . $result . " Wrong Username or Password";
+                 }
         ?>
     </body>
