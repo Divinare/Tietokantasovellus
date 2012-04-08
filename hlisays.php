b38ce8764339b0c10d219571e2a3461fe8874922
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-04-08 19:38:40 +0300

    lul

--- hlisays.php
+++ hlisays.php
@@ -2,14 +2,14 @@
 <!DOCTYPE html>
 
 <head>
-   <title>"Opettajan Lisäys"</title>
+   <title>Opettajan Lisäys</title>
    <meta charset="utf-8">
 </head>
 
    <body>
    <h1>Opettajan Lisäys</h1>
 
-   <Form name ='vastaukset' Method ='Post' ACTION ='lisays.php?lisays=".$_GET["kysely"]."'>
+   <Form name ='henkilotiedot' Method ='Post' ACTION ='lisays.php?lisays'>
 
    <p>Opettajan etunimi:</p>
    <input type='text' name='etu'><p></br>
@@ -28,6 +28,8 @@
    <input type="radio" name="rooli" value="admin"> Admin </br>
    <input type="radio" name="rooli" value="laitoksen vastuuhenkilö"> Laitoksen Vastuuhenkilö </br>
 
+   <Input type = 'Submit' Name = 'submit' Value = 'Lähetä'>
+
    <?php
    // lisätään tiedot tietokantaan
    //$yhteys = db::getDB();
@@ -36,16 +38,18 @@
    //$laita->execute(array($henkiloid, $etu, $suku, $sposti, $passu, $rooli));
    //$nimi = $laita->fetch();
 
-   if (isset($etu)) {
-   echo "Et antanut etunimeä";
-   }
+   //if (isset($etu)) {
+   //echo "Et antanut etunimeä";
+   //}
+
+
    //if (strlen($etu) > 30) {
    //echo "Annoit liian pitkän etunimen";
    //}
    //else {
    //<Input type = 'Submit' Name = 'submit' Value = 'Lisää henkilö'>
    // }
-//16:57 <Purrrrrr> Sä voit tehä actioniks välisivun jossa on koodia tyyliin <?php if (empty($_POST['etu'])) { 
+//16:57 <Purrrrrr> Sä voit tehä actioniks välisivun jossa on koodia tyyliin <?php if (empty($_POST['etu'])) {
 //                 header('Location: lomakesivu.php?viesti=Etunimi%20puuttui'); die(); }
 //17:00 <Purrrrrr> Jos sen tekee tohon tyyliin, mitä ylhääl, niin se tulee muuttujaan $_GET['viesti']
 
---
+++ lomakesivu.php
@@ -0,0 +1,17 @@
+<?php require_once 'DB.php'; ?>
+<!DOCTYPE html>
+
+<head>
+   <title>"Virheilmo"</title>
+   <meta charset="utf-8">
+</head>
+
+<body>
+
+   <h2>Yksi tai useampi lomake oli tyhjä tai tiedot olivat liian pitkiä</h2>
+
+   <FORM METHOD="LINK" ACTION="hlisays.php">
+   <INPUT TYPE="submit" VALUE="Takaisin">
+   </FORM>
+
+</body>
