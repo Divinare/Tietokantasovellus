0b5f072f62020b478b2775e24caca2d08dda9b75
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-04-08 20:04:56 +0300

    henkilonlisayseitoimi

--- lisays.php
+++ lisays.php
@@ -40,14 +40,16 @@
    header('Location: lomakesivu.php?viesti=Salasana%liian%pitkä'); die();
    }
 
+   $lol1 = $_POST['etu'];
+   $lol2 = $_POST['suku'];
+   $lol3 = $_POST['sposti'];
+   $lol4 = $_POST['passu'];
+   $lol5 = $_POST['rooli'];
 
 // Lisätään tiedot tietokantaan:
-   $sql = 'INSERT INTO Henkilo values (?, ?, ?, ?, ?, ?)';
+   $sql = 'INSERT INTO Henkilo values (?, ?, ?, ?, ?)';
    $laita = $yhteys->prepare($sql);
    $laita->execute(array($_POST['etu'], $_POST['suku'], $_POST['sposti'], $_POST['passu'], $_POST['rooli']));
-
+//   $laita->execute(array($lol1, $lol2, $lol3, $lol4, lol5));
 ?>
-
-
-
 </body>
