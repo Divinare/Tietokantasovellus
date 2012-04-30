a2f8161f14463e7f1f0c7f1c1202afa7677df0df
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-04-30 21:16:58 +0300

    z

--- yhteenveto.php
+++ yhteenveto.php
@@ -10,17 +10,31 @@
 <body>
        <h2>Yhteenveto</h2></br>
        <?php
-             $yhteys = db::getDB();
+        $yhteys = db::getDB();
 
-             $kysely = 'SELECT kurssikyselyid, kknimi FROM Kurssikysely ORDER BY kknimi';
+        $sqlrooli = 'SELECT rooli FROM henkilo WHERE henkiloid = ?';
+        $sqlrooli2 = $yhteys->prepare($sqlrooli);
+        $sqlrooli2->execute(array($_GET["yhteenveto"]));
+        $rooli = $sqlrooli2->fetch();
+
+        if ($rooli[0] == vastuuhenkilö) {
+             print "Käynnissä olevat kurssikyselyt:<br>";
+             $kysely = 'SELECT kurssikyselyid, kknimi, esilla FROM Kurssikysely WHERE esilla = TRUE ORDER BY kknimi';
              foreach ($yhteys->query($kysely) as $tulos) {
-                print "<a href=luoyv.php?luoyv=".$tulos['kurssikyselyid']."&henkiloid=".$_GET['yhteenveto'].">".$tulos['kknimi']."</a>"."</br>";
+                print "<a href=luoyv.php?luoyv=".$tulos['kurssikyselyid']."&henkiloid=".$_GET['yhteenveto'].">".$tulos['kknimi']."</a>"."<br>";
              }
+             print "<br><br>"
+             print "Päättyneet kurssikyselyt:<br>";
+             $kysely2 = 'SELECT kurssikyselyid, kknimi, esilla, ollutEsilla FROM Kurssikysely WHERE esilla = FALSE and ollutEsilla = FALSE ORDER BY kknimi';
+             foreach ($yhteys->query($kysely2) as $tulos) {
+                print "<a href=luoyv.php?luoyv=".$tulos['kurssikyselyid']."&henkiloid=".$_GET['yhteenveto'].">".$tulos['kknimi']."</a>"."<br>";
+             }
+        }
 
-      $sqlrooli = 'SELECT rooli FROM henkilo WHERE henkiloid = ?';
-      $sqlrooli2 = $yhteys->prepare($sqlrooli);
-      $sqlrooli2->execute(array($_GET["yhteenveto"]));
-      $rooli = $sqlrooli2->fetch();
+        if ($rooli[0] == opettaja) {
+
+        }
+
       ?>
      <p><a href=<?php print $rooli[0]; ?>.php?<?php print $rooli[0]; ?>=<?php print $_GET['yhteenveto']; ?>><img src="nuoli.png" border="0" /></a></p>
 
