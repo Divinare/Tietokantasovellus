878d911bf2f746bae74f873b576ed8ebf55d4a32
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-05-02 19:07:13 +0300

    pah

--- yhteenveto.php
+++ yhteenveto.php
@@ -29,7 +29,23 @@ $yhteys = db::getDB();
                 print "Käynnissä olevat kurssikyselyt:<br>";
                 $kysely = 'SELECT kurssikyselyid, kknimi FROM Kurssikysely WHERE esilla = TRUE ORDER BY kknimi';
                 foreach ($yhteys->query($kysely) as $tulos) {
-                    print "<a href=luoyv.php?luoyv=" . $tulos['kurssikyselyid'] . "&henkiloid=" . $_GET['yhteenveto'] . ">" . $tulos['kknimi'] . "</a>" . "<br>";
+                    // Haetaan kurssikyselyyn liittyvä henkiloid ja kurssiid
+                    $sqltk = 'SELECT henkiloid, kurssiid FROM kurssikysely WHERE kurssikyselyid = ?';
+                    $sqltk2 = $yhteys->prepare($sqltk);
+                    $sqltk2->execute(array($tulos['kurssikyselyid']));
+	            $sqltk3 = $sqltk2->fetchAll();
+                    // Haetaan henkilön nimi henkiloID:n avulla (joka siis saadaan $sqltk3[0][0])
+                    $sqln = 'SELECT etunimi, sukunimi FROM henkilo WHERE henkiloid = ?';
+                    $sqln2 = $yhteys->prepare($sqln);
+                    $sqln2->execute(array($sqltk3[0][0]));
+                    $sqln3 = $sqln2->fetchAll();
+                    // Haetaan kurssin nimi, vuosi ja periodi kurssiID:n avulla (joka saadaan $sqltk3[0][1])
+                    $sqlnpv = 'SELECT nimi, vuosi, periodi FROM kurssi WHERE kurssiid = ?';
+                    $sqlnpv2 = $yhteys->prepare($sqlnpv);
+                    $sqlnpv2->execute(array($sqltk3[0][1]));
+                    $sqlnpv3 = $sqlnpv2->fetchAll();
+                    print "<a href=luoyv.php?luoyv=" . $tulos['kurssikyselyid'] . "&henkiloid=" . $_GET['yhteenveto'] . ">" . $tulos['kknimi'] . "</a>" .
+                    " (".$sqln3[0][0] . " " . $sqln3[0][1] . " - " . $sqlnpv3[0][0] . " - Vuosi: " .$sqlnpv3[0][1] . " Periodi: " .$sqlnpv3[0][2] . ")" . "<br>";
                 }
                 print "<br><br>";
                 print "Päättyneet kurssikyselyt:<br>";
