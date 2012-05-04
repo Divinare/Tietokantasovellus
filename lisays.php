8cfb7960308a25c80471071b15d1c6313b3b738d
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-05-04 15:54:33 +0300

    korjaus

--- lisays.php
+++ lisays.php
@@ -3,17 +3,7 @@
 require_once 'DB.php';
 session_start();
 $yhteys = db::getDB();
-?>
-<!DOCTYPE html>
 
-<head>
-    <link rel="stylesheet" type="text/css" href="tyylit.css" />
-    <title>Vahvistus</title>
-    <meta charset="utf-8">
-</head>
-
-<body>
-    <?php
     // Istuntotarkastus
     if ($_SESSION["ihminen"] == $_GET["lisays"]) {
 
@@ -71,62 +61,15 @@ $yhteys = db::getDB();
             header("Location: hlisays.php?hlisays=" . $_GET["lisays"] . "&viesti=salateitäsmää" . "&etu=" . $_POST['etu'] . "&suku=" . $_POST['suku'] . "&sähkö=" . $_POST['sposti']);
             die();
         }
-        ?>
-        // Tarkastukset ok!
-        // Siirretään tiedot session muuttujilla vahvista_henkilo.php:seen
-        $_SESSION["etu"] = $_POST["etu"];
-        $_SESSION["suku"] = $_POST["suku"];
-        $_SESSION["email"] = $_POST["sposti"];
-        $_SESSION["pw"] = $_POST["passu"];
-        header("Location: vahvista_henkilo.php?admin=" . $_GET["lisays"]);
+// Tarkastukset ok!
+// Siirretään tiedot session muuttujilla vahvista_henkilo.php:seen
+$_SESSION["etu"] = $_POST["etu"];
+$_SESSION["suku"] = $_POST["suku"];
+$_SESSION["email"] = $_POST["sposti"];
+$_SESSION["pw"] = $_POST["passu"];
+$_SESSION["rooli"] = $_POST["rooli"];
+header("Location: vahvista_henkilo.php?admin=" . $_GET["lisays"]);
 
-	<!-- Jos kaikki on kunnossa näytetään vielä tiedot, jotka täytyy vahvistaa -->
-        <h2>Vahvista alla olevat tiedot</h2>
-        <div class="box">
-            <table>
-                <tr>
-                    <td><b>ETUNIMI</b></td>
-                    <td><?php print htmlspecialchars($_POST['etu']); ?> </td>
-                </tr>
-                <tr>
-                    <td><b>SUKUNIMI</b></td>
-                    <td><?php print htmlspecialchars($_POST['suku']); ?> </td>
-                </tr>
-                <tr>
-                    <td><b>E-MAIL</b></td>
-                    <td><?php print htmlspecialchars($_POST['sposti']); ?></td>
-                </tr>
-                <tr>
-                    <!-- Printataan salasanan verran tähtiä -->
-                    <td><b>SALASANA</b></td>
-                    <td><?php
-    for ($i = 1; $i <= strlen($_POST['passu']); $i++) {
-        print '*';
-    }
-        ?> </td>
-                </tr>
-                <tr>
-                    <td><b>ROOLI</b></td>
-                    <td><?php print $_POST['rooli']; ?></td>
-                </tr>
-            </table>
-            <br>
-            <!-- Nappula, joka lähettää tiedot lomakelahetys.php:lle -->
-            <Form name ='tiedot' Method ='Post' ACTION ='lomakelahetys.php?lomakelahetys=<?php print $_GET['lisays']; ?>'>
-                <input type='hidden' name='etu' value='<?php print htmlspecialchars($_POST['etu']); ?>'>
-                <input type='hidden' name='suku' value='<?php print htmlspecialchars($_POST['suku']); ?>'>
-                <input type='hidden' name='sposti' value='<?php print htmlspecialchars($_POST['sposti']); ?>'>
-                <input type='hidden' name='passu' value='<?php print htmlspecialchars($_POST['passu']); ?>'>
-                <input type='hidden' name='rooli' value='<?php print htmlspecialchars($_POST['rooli']); ?>'>
-                <Input type = 'Submit' Name = 'submit' Value = 'Vahvista tiedot'>
-            </form>
-        </div>
-        <ul class="navbar">
-            <li><p><a href=admin.php?admin=<?php print $_GET['lisays']; ?>>Oma sivu</a></p></li>
-            <li><p><a href=index.php>Kirjaudu ulos</a></p></li>
-        </ul>
-    </body>
-    <?php
 // Istuntotarkistus failaa
 } else {
     header("Location: access_denied.php");
