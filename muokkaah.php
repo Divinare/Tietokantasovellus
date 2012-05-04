cb0238340407a45779f0b93c4d2cb82fac820f36
Joe J Niemi <joeniemi@svm-19.cs.helsinki.fi>
2012-05-04 12:35:26 +0300

    sisennys

--- muokkaah.php
+++ muokkaah.php
@@ -73,9 +73,9 @@ $yhteys = db::getDB();
                             if ($_GET["viesti"] == emailfail) {
                                 print "<font color='red'>Sähköpostin on oltava 1-60 merkkiä pitkä.</font>";
                             }
-			    if ($_GET["viesti"] == emailkaytossa) {
-				print "<font color='red'>Sähköposti on jo käytössä.</font>";
-			    }
+                            if ($_GET["viesti"] == emailkaytossa) {
+                                print "<font color='red'>Sähköposti on jo käytössä.</font>";
+                            }
                             ?>
                         </form>
                     </td>
@@ -103,39 +103,39 @@ $yhteys = db::getDB();
                 <input type="radio" name="rooli" value="admin"> <?php print $admin; ?> </br>
                 <input type="radio" name="rooli" value="vastuuhenkilö"> <?php print $vastuuhenkilö; ?> </br>
                 <Input type = 'Submit' Name = 'submit' Value = 'Vaihda'> </br>
-    <?php
-    // Virheilmoitukset ja niiden käsittely
-    if ($_GET["viesti"] == omafail) {
-        print "<font color='red'>Et voi muokata omaa rooliasi.<font color='black'>";
-    }
-    if ($_GET["viesti"] == "emptyradiofail") {
-        print "<font color='red'>Et valinnut roolia.<font color='black'>";
-    }
-    ?>
+                <?php
+                // Virheilmoitukset ja niiden käsittely
+                if ($_GET["viesti"] == omafail) {
+                    print "<font color='red'>Et voi muokata omaa rooliasi.<font color='black'>";
+                }
+                if ($_GET["viesti"] == "emptyradiofail") {
+                    print "<font color='red'>Et valinnut roolia.<font color='black'>";
+                }
+                ?>
             </form>
             <br>
             <!-- Henkilön poisto -->
             <form action="poistah.php?admin=<?php print $_GET['admin']; ?>&henkiloid=<?php print $_GET['henkiloid']; ?>" method="post">
                 <input type="password" name="poista"> <input type="submit" value = "Poista henkilö"> (syötä salasanasi)
-    <?php
-    // Virheilmoitukset ja niiden käsittely
-    if ($_GET["viesti"] == salafail) {
-        print "<font color='red'>Toiminto vaatii salasanasi.<font color='black'>";
-    }
-    if ($_GET["viesti"] == itsefail) {
-        print "<font color='red'>Et voi poistaa itseäsi.<font color='black'>";
-    }
-    ?>
+                <?php
+                // Virheilmoitukset ja niiden käsittely
+                if ($_GET["viesti"] == salafail) {
+                    print "<font color='red'>Toiminto vaatii salasanasi.<font color='black'>";
+                }
+                if ($_GET["viesti"] == itsefail) {
+                    print "<font color='red'>Et voi poistaa itseäsi.<font color='black'>";
+                }
+                ?>
             </form>
         </div>
         <ul class="navbar">
             <li><p><a href=admin.php?admin=<?php print $_GET["admin"]; ?>>Oma sivu</a></p></li>
         </ul>
-    <?php
+        <?php
 // Istuntotarkistus failaa
-} else {
-    header("Location: access_denied.php");
-    die();
-}
-?>
+    } else {
+        header("Location: access_denied.php");
+        die();
+    }
+    ?>
 </body>
